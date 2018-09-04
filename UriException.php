65c6e5e4a0ccc736a8d522277e15b90b9acc1a45
Danilo Andrade <mandrade.danilo@gmail.com>
2018-09-04 16:46:43 -0300

    add uri exception

---
+++ UriException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Copyright (c) 2018 Danilo Andrade
+ *
+ *  This file is part of the apli project.
+ *
+ *  @project apli
+ *  @file UriException.php
+ *  @author Danilo Andrade <danilo@webbingbrasil.com.br>
+ *  @date 04/09/18 at 09:11
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Danilo
+ * Date: 04/09/2018
+ * Time: 09:11
+ */
+
+namespace Apli\Uri;
+
+use Exception;
+
+class UriException extends Exception
+{
+}
